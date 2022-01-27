83343b122915fff0057242e3db02a94c6806861c
Paweł Śniadała <biner.home@gmail.com>
2022-01-27 16:16:49 +0100

    Updates

--- app/src/components/PageHeader.js
+++
@@ -1,8 +0,0 @@
-const PageHeader = ({ header }) => (
-    <div className="page-header">
-        <h1 className="header">{header}</h1>
-        <hr className="divider" />
-    </div>
-);
-
-export default PageHeader;
--- app/src/components/SectionHeader.js
+++
@@ -1,8 +0,0 @@
-const SectionHeader = ({ header }) => (
-    <div className="section-header">
-        <h2 className="header">{header}</h2>
-        <hr className="divider" />
-    </div>
-);
-
-export default SectionHeader;
