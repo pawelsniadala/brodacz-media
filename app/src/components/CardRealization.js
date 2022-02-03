eccd9022d7d364b4d1acd52b907490fc9373f18e
Paweł Śniadała <biner.home@gmail.com>
2022-02-04 00:49:05 +0100

    Extension of the cardRealization component

--- app/src/components/CardRealization.js
+++ app/src/components/CardRealization.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Iframe from "react-iframe";
 import ReactImageAppear from "react-image-appear";
 
@@ -5,11 +6,21 @@ const CardRealization = ({
     cardTitle,
     cardPhoto,
     cardPhotoSrc,
+    layoutRef,
+    onClick,
+
     cardVideo,
     cardVideoId,
     cardVideoUrl,
-    onClick,
-    layoutRef
+
+    cardThumbnaiVideo,
+    cardThumbnaiVideoSrc,
+    cardThumbnaiVideoAlt,
+    cardThumbnaiVideoTime,
+    cardThumbnaiVideoTitle,
+    cardThumbnaiVideoCompany,
+    cardThumbnaiVideoDate,
+    cardThumbnaiVideoPath
 }) => (
     <div className={`card realization ${cardPhoto ? "photo" : "video"}`}>
         {cardPhoto && (
@@ -36,6 +47,45 @@ const CardRealization = ({
                 allowfullscreen
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             />
+        )}
+        {cardThumbnaiVideo && (
+            <div className="thumbnail">
+                <div className="img-box">
+                    <ReactImageAppear
+                        className="card-img-top"
+                        src={cardThumbnaiVideoSrc}
+                        alt={cardThumbnaiVideoAlt}
+                        placeholderStyle={{ backgroundColor: "transparent", display: "inline" }}
+                        showLoader={false}
+                    />
+                    <span className="card-video-time badge bg-dark">
+                        {cardThumbnaiVideoTime}
+                    </span>
+                </div>
+                <div className="card-body">
+                    <h6 className="card-title">
+                        {cardThumbnaiVideoTitle}
+                    </h6>
+                    <small className="card-meta text-muted">
+                        <span className="company">
+                            {cardThumbnaiVideoCompany}
+                        </span>
+                        <span className="company">
+                            {cardThumbnaiVideoDate}
+                        </span>
+                    </small>
+                    {/* <Link
+                        className="stretched-link"
+                        to={cardThumbnaiVideoPath ? cardThumbnaiVideoPath : "#"}
+                    >
+                    </Link> */}
+                    <a className="stretched-link"
+                        href={cardThumbnaiVideoPath ? cardThumbnaiVideoPath : "#"}
+                        target="_blank" rel="noopener noreferrer"
+                    >
+                    </a>
+                </div>
+            </div>
         )}
     </div>
 );
