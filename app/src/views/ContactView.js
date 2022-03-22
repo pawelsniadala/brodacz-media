efdd0cc3a5dbc481af4d1e100ed1ee02162c6282
Paweł Śniadała <biner.home@gmail.com>
2022-03-22 08:02:56 +0100

    Changing the structure of the contact view

--- app/src/views/ContactView.js
+++ app/src/views/ContactView.js
@@ -13,6 +13,7 @@ const ContactView = () => {
         e.preventDefault();
 
         emailjs.sendForm("service_uu7euve", "template_bnuaglf", form.current, "nCwtxRyHnXpW-ROrH")
+        // emailjs.sendForm("service_hgver59", "template_kidfc2k", form.current, "_o3zGPDB_96l97OYE")
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
@@ -25,21 +26,25 @@ const ContactView = () => {
         <div className="contact-view">
             <div className="view-wrapper container">
                 <PageHeader header="Kontakt" />
-                <form className="col-md-6" ref={form} onSubmit={sendEmail}>
-                    <div className="mb-3">
-                        <label htmlFor="name" className="form-label">Imię</label>
-                        <input type="text" className="form-control" name="name" />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email</label>
-                        <input type="email" className="form-control" name="email" />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="message" className="form-label">Wiadomość</label>
-                        <textarea className="form-control" name="message" rows="6"></textarea>
-                    </div>
-                    <button type="submit" className="btn btn-secondary btn-lg">Wyślij</button>
-                </form>
+                <div className="row">
+                    <form className="col-md-6" ref={form} onSubmit={sendEmail}>
+                        <div className="mb-3">
+                            <label htmlFor="name" className="form-label">Twoje imię</label>
+                            <input type="text" className="form-control" name="name" minLength="2" required />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Twój adres e-mail</label>
+                            <input type="email" className="form-control" name="email" required />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="message" className="form-label">Wiadomość</label>
+                            <textarea className="form-control" name="message" rows="6" required></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-secondary btn-lg button-submit">
+                            Wyślij
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     );
