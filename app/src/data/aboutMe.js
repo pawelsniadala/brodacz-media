db870866c887dcf39f13202aa4ebefcdfc3ad236
Paweł Śniadała <biner.home@gmail.com>
2022-03-22 08:03:38 +0100

    Adding text to the about me section

--- app/src/data/aboutMe.js
+++ app/src/data/aboutMe.js
@@ -4,7 +4,7 @@ export const aboutMe = [
     {
         id: "ccc80bbe-e212-4459-a7e4-7061dff6c06e",
         title: "O mnie",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        description: "Cześć! Od kilku lat tworzę zdjęcia i filmy, zdobywałem swoje doświadczenie w studiu fotograficzno-filmowym. Podczas pracy na dużych planach zdjęciowych mogłem nauczyć się od najlepszych swojego fachu, który staram się szlifować każdego dnia. Moją ulubionym aspektem tej pracy jest wszechstronność, dlatego jeżeli szukasz kompleksowej obsługi od opracowania koncepcji po realizację treści i ich odpowiednie pozycjonowanie w sieci, to jesteś we właściwym miejscu. Od zawsze byłem ekstrawertykiem, dlatego budowanie odpowiedniej relacji z klientem jest dla mnie szczególnie przyjemne i poświęcam temu aspektowi wiele czasu.",
         image: aboutMePhoto,
         link: "/videos",
         tab: "video-weddings"
