316a302133ddadd515134fdadb7a03be6c1bc7bf
Paweł Śniadała <biner.home@gmail.com>
2022-04-08 03:14:00 +0200

    Adding information about the number of photos in the Photo section

--- app/src/sections/PhotoSection.js
+++ app/src/sections/PhotoSection.js
@@ -3,29 +3,56 @@ import CardCategory from "../components/CardCategory";
 import Alert from "../components/Alert";
 import { photo } from "../data/photo";
 
-const PhotoSection = () => (
-    <section className="video-section">
-        <div className="container">
-            <div className="section-wrapper">
-                <SectionHeader header={photo.title} />
-                <div className="card-wrapper category photo">
-                    {photo.categories.length ? photo.categories.map(item => (
-                        <CardCategory
-                            key={item.id}
-                            cardTitle={item.title}
-                            cardPath={item.path}
-                            cardImg={item.image}
-                        />
-                    )) : (
-                        <Alert
-                            type="alert-light"
-                            content="Brak danych"
-                        />
-                    )}
+const PhotoSection = () => {
+    const cardRealizations = (title) => {
+        switch(title) {
+            case "Wizerunkowe":
+                return (
+                    photo.realization.branding.length
+                );
+            case "Studyjne":
+                return (
+                    photo.realization.studio.length
+                );
+            case "Produktowe":
+                return (
+                    photo.realization.product.length
+                );
+            case "Plenerowe":
+                return (
+                    photo.realization.outdoor.length
+                );
+            default:
+        }
+    }
+
+    return (
+        <section className="video-section">
+            <div className="container">
+                <div className="section-wrapper">
+                    <SectionHeader header={photo.title} />
+                    <div className="card-wrapper category photo">
+                        {photo.categories.length ? photo.categories.map(item => (
+                            <CardCategory
+                                key={item.id}
+                                cardTitle={item.title}
+                                cardDescription={item.description}
+                                cardLabel={item.label}
+                                cardPath={item.path}
+                                cardImg={item.image}
+                                cardRealizations={cardRealizations(item.title)}
+                            />
+                        )) : (
+                            <Alert
+                                type="alert-light"
+                                content="Brak danych"
+                            />
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-);
+        </section>
+    );
+}
 
 export default PhotoSection;
