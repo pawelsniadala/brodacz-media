ef656f38584254d3c65f2bc789c7b785ccd271f0
Paweł Śniadała <biner.home@gmail.com>
2022-04-08 03:19:09 +0200

    Editing the CardCategory component

--- app/src/components/CardCategory.js
+++ app/src/components/CardCategory.js
@@ -4,24 +4,45 @@ import ReactImageAppear from "react-image-appear";
 const CardCategory = ({
     cardImg,
     cardTitle,
-    cardPath
+    cardDescription,
+    cardLabel,
+    cardPath,
+    cardRealizations
 }) => (
 
+
     <div className="card category">
-        <ReactImageAppear
+        <img src={cardImg} className="card-img-top" alt={cardTitle} />
+
+        {/* <ReactImageAppear
+            className="card-img-top"
             src={cardImg}
             alt={cardTitle}
             placeholderStyle={{ backgroundColor: "transparent", display: "inline" }}
             showLoader={false}
-        />
-        <div className="card-title-wrapper" />
-        <div className="card-title">
-            <h3>{cardTitle}</h3>
+        /> */}
+
+        <div className="card-body">
+            <h5 className="card-title">
+                {cardTitle}
+            </h5>
+            <p className="card-text">
+                {cardDescription}
+            </p>
+            {/* <a href="#" class="btn btn btn-success">Zobacz realizacje</a> */}
+            {/* <div className="d-grid"> */}
+            <button type="button" className="button-card">
+                Zobacz {cardLabel}
+            </button>
+            {/* </div> */}
+            {/* <button type="button" class="btn btn-light">Zobacz realizacje</button> */}
+        </div>
+        <div className="card-footer text-muted">
+            Realizacje • {cardRealizations}
         </div>
         <Link
             className="stretched-link"
-            to={cardPath ? cardPath : "#"}
-        >
+            to={cardPath ? cardPath : "#"}>
         </Link>
     </div>
 );
