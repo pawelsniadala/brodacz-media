8b8b1135e7e6f26100aad5cd204a85ea587f4e7c
Paweł Śniadała <biner.home@gmail.com>
2022-04-21 23:15:24 +0200

    Added Feedback section

---
+++ app/src/data/feedback.js
@@ -0,0 +1,84 @@
+import feedbackInformation from '../assets/feedback/information/feedbackInformation.png';
+
+//import recommendationProfilePhotoAlternative from '../assets/feedback/recommendationProfilePhotoAlternative.png';
+
+import recommendationProfilePhoto1 from '../assets/feedback/opinions/recommendationProfilePhoto1.jpg';
+import recommendationBackgroundPhoto1 from '../assets/feedback/opinions/recommendationBackgroundPhoto1.jpg';
+
+import recommendationProfilePhoto2 from '../assets/feedback/opinions/recommendationProfilePhoto2.jpg';
+import recommendationBackgroundPhoto2 from '../assets/feedback/opinions/recommendationBackgroundPhoto2.jpg';
+
+
+export const feedback = {
+    "information": [
+        {
+            id: "207c3d9c-dde5-4489-b8af-46b1496f74dd",
+            title: 'Julien Mauve',
+            description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta consequat nulla eget iaculis. Fusce dictum diam a mauris condimentum feugiat. Aliquam accumsan consectetur ultricies. Maecenas malesuada bibendum gravida."',
+            image: feedbackInformation
+        }
+    ],
+    "opinions": [
+        {
+            id: "8277130f-383c-4d10-bee3-953f11c4f790",
+            name: 'Julien Mauve',
+            position: 'Fotograf',
+            description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta consequat nulla eget iaculis. Fusce dictum diam a mauris condimentum feugiat. Aliquam accumsan consectetur ultricies. Maecenas malesuada bibendum gravida."',
+            rating: 5,
+            image: {
+                profile: recommendationProfilePhoto1,
+                background: recommendationBackgroundPhoto1
+            }
+        },
+        {
+            id: "f1324f71-85bd-42af-b721-b368d8bb727a",
+            name: 'Mateusz Morawiecki',
+            position: 'Prezes Rady Ministrów',
+            description: '"Proin at odio volutpat, finibus neque quis, venenatis ex. Duis semper ultricies condimentum. Sed volutpat pulvinar quam, quis vehicula turpis ornare at. Etiam placerat tincidunt turpis ut tristique. Sed mi lacus, accumsan gravida magna eget, sagittis volutpat ipsum. Maecenas a iaculis lacus, nec consequat urna. Donec lacinia sit amet libero vel maximus."',
+            rating: 5,
+            image: {
+                profile: recommendationProfilePhoto2,
+                background: recommendationBackgroundPhoto2
+            }
+        },
+        {
+            id: "c28ecbc9-7509-442c-8098-6442405b17b6",
+            name: 'Julien Mauve',
+            position: 'Fotograf',
+            description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta consequat nulla eget iaculis. Fusce dictum diam a mauris condimentum feugiat. Aliquam accumsan consectetur ultricies. Maecenas malesuada bibendum gravida."',
+            rating: 5,
+            image: {
+                profile: recommendationProfilePhoto1,
+                background: recommendationBackgroundPhoto1
+            }
+        },
+        {
+            id: "a6817228-3c44-4a8c-9013-018f6816fcff",
+            name: 'Mateusz Morawiecki',
+            position: 'Prezes Rady Ministrów',
+            description: '"Proin at odio volutpat, finibus neque quis, venenatis ex. Duis semper ultricies condimentum. Sed volutpat pulvinar quam, quis vehicula turpis ornare at. Etiam placerat tincidunt turpis ut tristique. Sed mi lacus, accumsan gravida magna eget, sagittis volutpat ipsum. Maecenas a iaculis lacus, nec consequat urna. Donec lacinia sit amet libero vel maximus."',
+            rating: 5,
+            image: {
+                profile: recommendationProfilePhoto2,
+                background: recommendationBackgroundPhoto2
+            }
+        },
+    ]
+};
+
+
+
+    // {
+    //     id: "",
+    //     name: 'Andrzej Duda',
+    //     position: 'Prezydent Rzeczypospolitej Polskiej',
+    //     description: '"Integer porta orci sed felis accumsan consequat. Suspendisse lacinia, neque posuere facilisis iaculis, leo turpis sagittis ipsum, ut sodales erat est in tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."',
+    //     image: recommendationPhoto1
+    // },
+    // {
+    //     id: "81aaa185-5191-4943-a230-e4b6c6a8b3cb",
+    //     name: 'Janusz Korwin-Mikke',
+    //     position: 'Poseł',
+    //     description: '"Proin tincidunt sem ac dolor posuere fermentum. Sed molestie dui sed metus fermentum posuere. In feugiat id mauris at pellentesque. Pellentesque efficitur nisi justo, at suscipit augue tempor ut. Vivamus egestas nisi id consequat semper. Sed tellus lectus, pellentesque eu sodales laoreet, consequat sodales lorem. Nam justo justo, semper eu nisi vel, viverra volutpat risus."',
+    //     image: recommendationPhoto1
+    // },
---
+++ app/src/sections/FeedbackSection.js
@@ -0,0 +1,67 @@
+import { feedback } from "../data/feedback";
+import ReactImageAppear from "react-image-appear";
+import ReactStars from "react-stars";
+
+const FeedbackSection = () => (
+    <section className="feedback-section">
+        <div className="container">
+            <div className="section-wrapper">
+                {feedback.information.length && feedback.information.map(item => (
+                    <div key={item.id} className="card feedback information">
+                    <ReactImageAppear
+                            src={item.image}
+                            className="card-img"
+                            alt="..."
+                            placeholderStyle={{ backgroundColor: "transparent", display: "inline" }}
+                            showLoader={false}
+                        />
+                        <div className="text-wrapper">
+                            <div className="heading">
+                                Dołącz do grona<br/>
+                                zadowolonych klientów
+                            </div>
+                            <div className="description">
+                                Będę bardzo wdzięczny<br/>
+                                jeśli podzielisz się swoją opinią<br/>
+                                na temat moich usług.
+                            </div>
+                            <button type="button" className="button-card">
+                                Napisz opinię
+                            </button>
+                        </div>
+                    </div>
+                ))}
+                <div className="card-wrapper feedback opinions">
+                    {feedback.opinions.length && feedback.opinions.map(item => (
+                        <div key={item.id} className="card feedback opinions">
+                            <div className="card-body">
+                                <div className="profile">
+                                    <img src={item.image.profile} alt={item.name} />
+                                </div>
+                                <div className="name">
+                                    {item.name}
+                                </div>
+                                <div className="position">
+                                    {item.position}
+                                </div>
+                                <div className="description">
+                                    {item.description}
+                                </div>
+                                <ReactStars
+                                    className="rating"
+                                    edit={false}
+                                    value={item.rating}
+                                    size={24}
+                                />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </section>
+);
+
+export default FeedbackSection;
+
+// https://github.com/voronianski/react-star-rating-component/issues/63
