30888f320352b84519d6edacc3a0b371e49a8352
Paweł Śniadała <biner.home@gmail.com>
2022-04-26 02:27:58 +0200

    Data update for the Customer section

--- app/src/data/customer.js
+++ app/src/data/customer.js
@@ -1,15 +1,16 @@
-// import customerPhoto1 from '../assets/customer/customerPhoto1.png';
+import customerPhoto1 from '../assets/customer/customerPhoto1.png';
 import customerPhoto2 from '../assets/customer/customerPhoto2.png';
 import customerPhoto3 from '../assets/customer/customerPhoto3.png';
 import customerPhoto4 from '../assets/customer/customerPhoto4.png';
 import customerPhoto5 from '../assets/customer/customerPhoto5.png';
+import customerPhoto6 from '../assets/customer/customerPhoto6.png';
 
 export const customer = [
-    // {
-    //     id: "a33e29eb-f3e6-4069-b32f-861ea628f5ca",
-    //     name: 'Erobocze Show',
-    //     image: customerPhoto1
-    // },
+    {
+        id: "a33e29eb-f3e6-4069-b32f-861ea628f5ca",
+        name: 'Erobocze Show',
+        image: customerPhoto1
+    },
     {
         id: "af476ccb-961d-4870-a23e-ff18d10adac0",
         name: 'Robocze Pracuj Profesjonalnie News',
@@ -20,14 +21,19 @@ export const customer = [
         name: 'Las Media',
         image: customerPhoto3
     },
-    {
-        id: "71e84fe0-c6d2-4245-85ba-5989fff7dea8",
-        name: 'Erobocze.pl',
-        image: customerPhoto4
-    },
+    // {
+    //     id: "71e84fe0-c6d2-4245-85ba-5989fff7dea8",
+    //     name: 'Erobocze.pl',
+    //     image: customerPhoto4
+    // },
     {
         id: "26b86551-14c8-4a87-9d6a-a906d269b4fe",
         name: 'Manager Business Hub',
         image: customerPhoto5
+    },
+    {
+        id: "7f6b249d-3df9-4040-80a2-a745de9de3f7",
+        name: 'Manager Business Hub',
+        image: customerPhoto6
     }
 ];
