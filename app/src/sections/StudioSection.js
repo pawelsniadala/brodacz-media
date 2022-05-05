ef9d460aad9bfc401a14cf44ec623d6b29a35a11
Paweł Śniadała <biner.home@gmail.com>
2022-05-05 16:16:12 +0200

    Adding a wrapper to the Studio section

--- app/src/sections/StudioSection.js
+++ app/src/sections/StudioSection.js
@@ -3,30 +3,32 @@ import Parallax from "../components/Parallax";
 
 const StudioSection = () => (
     <section className="studio-section">
-        <Parallax section="studio">
-            <div className="parallax-content">
-                <div className="heading">
-                    Zobacz siebie<br/>
-                    w innym świetle
+        <div className="section-wrapper">
+            <Parallax section="studio">
+                <div className="parallax-content">
+                    <div className="heading">
+                        Zobacz siebie<br/>
+                        w innym świetle
+                    </div>
+                    <div className="button-wrapper">
+                        <Link
+                            to="/contact"
+                            className="button studio"
+                            aria-current="page"
+                        >
+                            Zapisz się na sesję
+                        </Link>
+                        <Link
+                            to="/photo/studio"
+                            className="button studio photo"
+                            aria-current="page"
+                        >
+                            Zobacz zdjęcia
+                        </Link>
+                    </div>
                 </div>
-                <div className="button-wrapper">
-                    <Link
-                        to="/contact"
-                        className="button studio"
-                        aria-current="page"
-                    >
-                        Zapisz się na sesję
-                    </Link>
-                    <Link
-                        to="/photo/studio"
-                        className="button studio photo"
-                        aria-current="page"
-                    >
-                        Zobacz zdjęcia
-                    </Link>
-                </div>
-            </div>
-        </Parallax>
+            </Parallax>
+        </div>
     </section>
 );
 
