030e4fd75298ffab6664ad4adce4308d2c51601a
Paweł Śniadała <biner.home@gmail.com>
2022-05-07 01:37:09 +0200

    Adding a button to the Outdoor section

--- app/src/sections/OutdoorSection.js
+++ app/src/sections/OutdoorSection.js
@@ -9,13 +9,22 @@ const OutdoorSection = () => (
                     Chodźmy na spacer<br/>
                     zrobimy kilka zdjęć<br/>
                 </div>
+                <div className="button-wrapper">
                 <Link
                     to="/contact"
-                    className="button studio"
+                    className="button outdoor"
                     aria-current="page"
                 >
                     Zapisz się na sesję
                 </Link>
+                <Link
+                    to="/photo/outdoor"
+                    className="button outdoor photo"
+                    aria-current="page"
+                >
+                    Zobacz zdjęcia
+                </Link>
+                </div>
             </div>
         </Parallax>
     </section>
