3f45759382207287c525c9990f6a5828b6e1d20d
Paweł Śniadała <biner.home@gmail.com>
2023-01-04 07:32:14 +0100

    Podcast update

--- app/src/data/podcast.js
+++ app/src/data/podcast.js
@@ -1,11 +1,23 @@
 import podcastThumbnail1 from "../assets/podcast/podcastThumbnail1.png";
 import podcastThumbnail2 from "../assets/podcast/podcastThumbnail2.png";
+import podcastThumbnail3 from "../assets/podcast/podcastThumbnail3.png";
 
 export const podcast = {
     title: "Podcasty",
     path: "/podcasts",
     empty: "Brak video.",
     realization: [
+        {
+            id: "0df39b61-4dfc-4e42-93b5-9b82c86fd54d",
+            date: "2 sty 2023",
+            title: "Co musisz wiedzieć przed założeniem e-commerce. Rozmowa z Mikołajem Drgasem",
+            company: "PKO Bank Polski",
+            podcast: {
+                time: "41:15",
+                src: podcastThumbnail3,
+                urlPage: "https://www.youtube.com/watch?v=fuDaTIy6atA"
+            }
+        },
         {
             id: "762bed7a-0502-4b14-8d83-7c835f859aa8",
             date: "17 sie 2022",
